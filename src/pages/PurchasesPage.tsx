890fbdd7c5eff92519586d553a8eac8323c52596
Sudhanshu Singh <99180034+singhsudhanshu075@users.noreply.github.com>
2025-06-07 05:42:16 +0530

    Updated SalesPage.tsx

--- src/pages/PurchasesPage.tsx
+++ src/pages/PurchasesPage.tsx
@@ -27,8 +27,8 @@ const PurchasesPage: React.FC = () => {
         <div className="bg-green-50 border-l-4 border-green-400 p-4 rounded-md">
           <div className="flex">
             <div className="flex-shrink-0">
-              <svg className="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+              <svg className="h-5 w-5 text-green-400\" xmlns="http://www.w3.org/2000/svg\" viewBox="0 0 20 20\" fill="currentColor">
+                <path fillRule="evenodd\" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z\" clipRule="evenodd" />
               </svg>
             </div>
             <div className="ml-3">
